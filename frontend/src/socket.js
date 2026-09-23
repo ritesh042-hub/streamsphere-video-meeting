464dba2476d79d3da7bac0105e84fb33bee66483
Ritesh <itsmeeeritesh@gmail.com>
2026-09-23 18:59:55 +0530

    Connect frontend to deployed backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://streamsphere-backend-ubio.onrender.com";
 
 export const socket = io(API_URL, {
   autoConnect: true,
